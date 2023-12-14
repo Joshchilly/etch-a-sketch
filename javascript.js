a278c363cdaa0d36dccdeb705497f5cf21156268
Josh Isaacson <65214680+Joshchilly@users.noreply.github.com>
2023-12-14 14:33:08 -0500

    Build basic Etch A Sketch
    - built HTML skeleton of website
    - made significant progress in styling game in CSS
    - added fundamental JS logic for basic interactivity of game

--- javascript.js
+++ javascript.js
@@ -1,8 +1,38 @@
-const grid = document.querySelector('.grid-container');
+const grid = document.querySelector('.grid');
 
 for (i = 0; i < 256; i++) {
     const square = document.createElement('div');
-    square.setAttribute('style', 'flex: 1 1 auto; width: 40px; height: 40px; background-color: pink; border: 2px solid black;');
+    square.classList.add('square');
     grid.appendChild(square);
 }
 
+let mouseIsDown = false;
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => {
+    square.addEventListener('mousedown', () => {
+        mouseIsDown = true;
+        square.setAttribute('style', 'background-color: black;');
+    });
+
+    square.addEventListener('click', () => {
+        square.setAttribute('style', 'background-color: black;');
+    });
+
+
+    square.addEventListener('mousemove', () => {
+        if (mouseIsDown) {
+            square.setAttribute('style', 'background-color: black;');
+        }
+    });
+});
+
+grid.addEventListener('mouseup', () => {
+    mouseIsDown = false;
+});
+
+const body = document.querySelector('body');
+
+grid.addEventListener('mouseleave', () => {
+    mouseIsDown = false;
+});
