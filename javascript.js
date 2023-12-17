fb3dd3c5f26c75a092d4a87a383a999de09f3985
Josh Isaacson <65214680+Joshchilly@users.noreply.github.com>
2023-12-16 21:50:55 -0500

    Finish input box and update button
    - added and tested interactivity and logic of the input box and
      update button

--- javascript.js
+++ javascript.js
@@ -2,47 +2,66 @@ const body = document.querySelector('body');
 const grid = document.querySelector('.grid');
 const eraser = document.querySelector('.eraser');
 const buttons = document.querySelectorAll('button');
+const inputBox = document.querySelector('#custom-size-input');
+const updateButton = document.querySelector('.update');
 const classicButton = document.querySelector('.classic');
 const randomButton = document.querySelector('.random');
 const rainbowButton = document.querySelector('.rainbow');
 const eraserButton = document.querySelector('.eraser');
 const clearButton = document.querySelector('.clear');
-let mouseIsDown = false, eraserOn = false, classicOn = true, randomOn = false, rainbowOn = false;
-let randomColor = null, rainbowColor = null;
+let mouseIsDown = false, inputBoxClicked = false, classicOn = true, randomOn = false, rainbowOn = false, eraserOn = false;
+let squares = null, randomColor = null, rainbowColor = null, currentTimeout = null;
+let small = 4, medium = 16, big = 256;
 
-for (i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    grid.appendChild(square);
+buildGrid(medium);
+
+function buildGrid(squaresPerSide) {
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+
+    /* 650px is grid width/height, dividing by squaresPerSide and setting the result to width/height of each square
+       makes sure that the grid is entirely and exactly filed up by all the squares */
+    let newWidthAndHeight = 650 / squaresPerSide;
+    for (i = 0; i < (squaresPerSide ** 2); i++) {
+        const square = document.createElement('div');
+        square.style.width = `${newWidthAndHeight}px`;
+        square.style.height = `${newWidthAndHeight}px`;
+        square.classList.add('square');
+        grid.appendChild(square);
+    }
+
+    configureGrid();
 }
 
-const squares = document.querySelectorAll('.square');
+function configureGrid() {
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mousedown', () => {
+            mouseIsDown = true;
+            eraserOn == false ? ink(square) : square.style.backgroundColor = '#D8D8D8';
+        });
 
-squares.forEach((square) => {
-    square.addEventListener('mousedown', () => {
-        mouseIsDown = true;
-        eraserOn == false ? ink(square) : square.setAttribute('style', 'background-color: #D8D8D8;');
+        square.addEventListener('mousemove', () => {
+            if (mouseIsDown) {
+                eraserOn == false ? ink(square) : square.style.backgroundColor = '#D8D8D8';
+            }
+        });
     });
-
-    square.addEventListener('mousemove', () => {
-        if (mouseIsDown) {
-            eraserOn == false ? ink(square) : square.setAttribute('style', 'background-color: #D8D8D8;');
-        }
-    });
-});
+}
 
 function ink(square) {
     if (rainbowOn) {
         const styles = window.getComputedStyle(square);
         if (styles.getPropertyValue('background-color') == 'rgb(216, 216, 216)') {
             rainbowColor = Math.floor(Math.random() * 16777215).toString(16);
-            square.style.cssText = `background-color: #${rainbowColor};`;
+            square.style.backgroundColor = `#${rainbowColor}`;
         }
 
         return;
     }
 
-    square.style.cssText = (randomOn == true) ? `background-color: #${randomColor};` : 'background-color: rgb(85, 82, 82);';
+    square.style.backgroundColor = (randomOn == true) ? `#${randomColor}` : 'rgb(85, 82, 82)';
 }
 
 grid.addEventListener('mouseup', () => {
@@ -51,16 +70,6 @@ grid.addEventListener('mouseup', () => {
 
 grid.addEventListener('mouseleave', () => {
     mouseIsDown = false;
-});
-
-eraser.addEventListener('click', (e) => {
-    if (eraserOn) {
-        e.target.textContent = 'Eraser';
-        eraserOn = false;
-    } else {
-        e.target.textContent = 'Pen';
-        eraserOn = true;
-    }
 });
 
 buttons.forEach((button) => {
@@ -77,14 +86,37 @@ buttons.forEach((button) => {
     });
 });
 
-clearButton.addEventListener('click', () => {
-    squares.forEach((square) => {
-        square.setAttribute('style', 'background-color: #D8D8D8;')
-    });
+inputBox.addEventListener('click', (e) => {
+    if (inputBoxClicked) {
+        return;
+    }
 
-    eraserOn = false;
-    eraserButton.textContent = 'Eraser';
+    e.target.value = "";
+    inputBoxClicked = true;
+    clearTimeout(currentTimeout);
 });
+
+updateButton.addEventListener('click', () => {
+    inputBoxClicked = false;
+    currentInput = inputBox.value;
+    if (isNaN(currentInput) || !Number.isInteger(+currentInput) || (currentInput < 1 || currentInput > 100)) {
+        if (currentInput == "" || inputBox.value == "enter number from 1-100") {
+            inputBox.value = "no number entered";
+        } else {
+            inputBox.value = "invalid input";
+        }
+
+        currentTimeout = window.setTimeout(fillDefaultInput, 1750);
+        return;
+    }
+
+    clearButton.click();
+    buildGrid(currentInput);
+});
+
+function fillDefaultInput() {
+    inputBox.value = "enter number from 1-100";
+}
 
 classicButton.addEventListener('click', (e) => {
     if (!classicOn) {
@@ -110,4 +142,24 @@ rainbowButton.addEventListener('click', () => {
     }
 
     classicOn = false, randomOn = false, rainbowOn = true;
+});
+
+eraser.addEventListener('click', (e) => {
+    if (eraserOn) {
+        e.target.textContent = 'Eraser';
+        eraserOn = false;
+    } else {
+        e.target.textContent = 'Pen';
+        eraserOn = true;
+    }
+});
+
+clearButton.addEventListener('click', () => {
+    squares.forEach((square) => {
+        square.style.backgroundColor = '#D8D8D8';
+    });
+
+    eraserOn = false, inputBoxClicked = false;
+    inputBox.value = "enter number from 1-100";
+    eraserButton.textContent = 'Eraser';
 });
